5e51517714b0176216e107d9ba153d8a980a44d7
Ian Graham <i4ng1999@gmail.com>
2024-01-19 14:47:36 +0000

    Complete api hosting 1-5 tasks

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,15 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
