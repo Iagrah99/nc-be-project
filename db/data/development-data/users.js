aca8f325389e8562a291437e78f2b8980f1836a6
Ian Graham <i4ng1999@gmail.com>
2025-04-25 16:53:51 +0100

    Update user image links

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,31 +2,37 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://i.ibb.co/h1VbbJmS/tomtickle.png',
+    avatar_url:
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596264/tomtickle_l7hyac.png',
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url: 'https://i.ibb.co/zHQr8Dc5/paulgrump.png',
+    avatar_url:
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596267/paulgrump_e4wv21.png',
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
-    avatar_url: 'https://i.ibb.co/KxdGs1Gb/happyamy2016.png',
+    avatar_url:
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596267/happyamy2016_tkuajq.png',
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url: 'https://i.ibb.co/7xXKLYzf/cooljmessy.png',
+    avatar_url:
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596267/cooljmessy_wlrojg.png',
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    avatar_url: 'https://i.ibb.co/Q3Jg5gW7/weegembump.png',
+    avatar_url:
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596263/weegembump_ooeais.png',
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
-    avatar_url: 'https://i.ibb.co/WW5RXqpn/jessjelly.png',
+    avatar_url:
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596265/jessjelly_r4bldy.png',
   },
 ];
