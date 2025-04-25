6f631faa8d1cab627f3daf82623694f0a80ac9d5
Ian Graham <i4ng1999@gmail.com>
2025-04-25 17:17:56 +0100

    Add compressed user image versions

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -3,36 +3,36 @@ module.exports = [
     username: 'tickle122',
     name: 'Tom Tickle',
     avatar_url:
-      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596264/tomtickle_l7hyac.png',
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745597600/tomtickle_l7hyac_gq6mrf.png',
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
     avatar_url:
-      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596267/paulgrump_e4wv21.png',
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745597600/paulgrump_e4wv21_cppfqc.png',
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
     avatar_url:
-      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596267/happyamy2016_tkuajq.png',
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745597600/happyamy2016_tkuajq_q8vzk5.png',
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
     avatar_url:
-      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596267/cooljmessy_wlrojg.png',
+      'hhttps://res.cloudinary.com/dafsdsmus/image/upload/v1745597601/cooljmessy_wlrojg_ha7ccl.png',
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
     avatar_url:
-      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596263/weegembump_ooeais.png',
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745597599/weegembump_ooeais_y7o50q.png',
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
     avatar_url:
-      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745596265/jessjelly_r4bldy.png',
+      'https://res.cloudinary.com/dafsdsmus/image/upload/v1745597600/jessjelly_r4bldy_pi9oce.png',
   },
 ];
